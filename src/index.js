fd220a09dc63aa0d6eb7dd199b7836f252a2c4b9
Sujal7ss <sujalchahande3@gmail.com>
2024-06-19 19:12:00 +0530

    removed static mode

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/output.css';
-import './styles/index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import toast, { Toaster } from 'react-hot-toast';
-import Modal from 'react-modal';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-Modal.setAppElement('#root');
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/output.css";
+import "./styles/index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import toast, { Toaster } from "react-hot-toast";
+import Modal from "react-modal";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+Modal.setAppElement("#root");
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <>
     <App />
     <Toaster />
-  </React.StrictMode>
+  </>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
