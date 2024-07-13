9becf983e51701df73ee157ce2318b243b10bd69
Sujal7ss <sujalchahande3@gmail.com>
2024-07-13 12:42:04 +0530

    Added logout in mobile view

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -148,6 +148,15 @@ export default function Navbar({ pages }) {
                 {item.name}
               </NavLink>
             ))}
+            {pages !== "Homepage" && (
+              <button
+                key={10}
+                className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                onClick={handleLogout}
+              >
+                Logout
+              </button>
+            )}
 
             {button.name === "Profile" && (
               <Button
