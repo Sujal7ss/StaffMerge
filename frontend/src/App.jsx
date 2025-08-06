e5e4d7279cd2f6db9a2929540b380970aaba5b81
Sujal7ss <sujalchahande3@gmail.com>
2025-08-06 09:35:32 +0530

    Added protected route to jobDescription

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -34,7 +34,7 @@ const appRouter = createBrowserRouter([
   },
   {
     path: "/description/:id",
-    element: <JobDescription />
+    element: <ProtectedRoute><JobDescription /></ProtectedRoute>
   },
   {
     path: "/browse",
